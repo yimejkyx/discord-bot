c3d2e3acdb54e47dcf5c22760a54e300c352900a
Nikolas Tomaštík <nikolas.tsk@gmail.com>
2020-04-03 03:27:43 +0200

    changed member log

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("message", async msg => {
 
   if (member) {
     const { username } = member.user;
-    logger.info(`got member`, { username, content });
+    logger.info(`message got member ${{ username, content }}`);
   }
 
   if (content === "help") {

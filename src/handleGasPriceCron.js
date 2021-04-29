f6df6e5623e9687767ee982f49ccf18c1e0cba1d
Nikolas Tomastik <nikolas.tsk@gmail.com>
2021-04-29 19:53:44 +0200

    Fixing cron job error catching

--- src/handleGasPriceCron.js
+++ src/handleGasPriceCron.js
@@ -12,33 +12,37 @@ const {defiPulseApiKey, cmdPrefix} = require("../config.json");
 async function handleGasPriceCron(client, gasState) {
     logger.info('CRON: started cron job gas prices');
     new CronJob('1 */10 * * * *', async () => {
-        logger.info('CRON TRIGGER: Fetching gas price every 10 minutes');
+        try {
+            logger.info('CRON TRIGGER: Fetching gas price every 10 minutes');
 
-        const res = await axios.get(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${defiPulseApiKey}`);
-        const {data} = res;
-        const {fastest, fast, average, safeLow} = data;
-
-        logger.debug(`gas price data: ${JSON.stringify(data)}`);
-        gasState.prices = [
-            {...data, created: new Date()},
-            ...gasState.prices.slice(0, 144 - 1), // Hold gas prices for last day
-        ];
-        await fs.writeFile("./gasState.json", JSON.stringify(gasState));
-
-        gasState.listeners.forEach((listener) => {
-            if (average >= listener.limit * 10) return;
-
-            const guild = client.guilds.cache.find((guild) => guild.id === listener.guildId);
-            if (!guild) return;
-
-            const user = guild.members.cache.find((user) => user.id === listener.userId);
-            if (!user) return;
-
-            const channel = guild.channels.cache.find((channel) => channel.id === listener.channelId);
-            if (!channel) return;
-
-            channel.send(`<@${user.id}> ETH limit avg < ${listener.limit}: fastest **${fastest / 10}** (<30s), fast **${fast / 10}** (<2m), average **${average / 10}** (<5m), safe low **${safeLow / 10}** (<30m)`);
-        });
+            const res = await axios.get(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${defiPulseApiKey}`);
+            const {data} = res;
+            const {fastest, fast, average, safeLow} = data;
+    
+            logger.debug(`gas price data: ${JSON.stringify(data)}`);
+            gasState.prices = [
+                {...data, created: new Date()},
+                ...gasState.prices.slice(0, 144 - 1), // Hold gas prices for last day
+            ];
+            await fs.writeFile("./gasState.json", JSON.stringify(gasState));
+    
+            gasState.listeners.forEach((listener) => {
+                if (average >= listener.limit * 10) return;
+    
+                const guild = client.guilds.cache.find((guild) => guild.id === listener.guildId);
+                if (!guild) return;
+    
+                const user = guild.members.cache.find((user) => user.id === listener.userId);
+                if (!user) return;
+    
+                const channel = guild.channels.cache.find((channel) => channel.id === listener.channelId);
+                if (!channel) return;
+    
+                channel.send(`<@${user.id}> ETH limit avg < ${listener.limit}: fastest **${fastest / 10}** (<30s), fast **${fast / 10}** (<2m), average **${average / 10}** (<5m), safe low **${safeLow / 10}** (<30m)`);
+            });
+        } catch (err) {
+            logger.error(`gas price cron error: ${err.stack}`);
+        }
     }).start();
 }
 
